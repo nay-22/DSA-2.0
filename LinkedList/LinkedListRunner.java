5823f81c42cf452c1f4594629cdf2a30422346ef
Nayan <nayan92001@gmail.com>
2024-07-18 23:14:01 +0530

    Tested Linked List data removal functionalities

--- LinkedList/LinkedListRunner.java
+++ LinkedList/LinkedListRunner.java
@@ -13,8 +13,24 @@ public class LinkedListRunner {
         list.addStart(60);
         System.out.println(list);
         list.addAfter(35, 30);
+        System.out.println(list);
         list.addBefore(15, 20);
+        System.out.println(list);
         list.addBefore(70, 10);
+        System.out.println(list);
+        list.remove(15);
+        list.remove(35);
+        list.remove(35);
+        System.out.println(list);
+        System.out.println(list.removeStart());
+        System.out.println(list);
+        System.out.println(list.removeLast());
+        System.out.println(list);
+        System.out.println("REMOVE AFTER");
+        System.out.println(list.removeAfter(30));
+        System.out.println(list);
+        System.out.println("REMOVE BEFORE");
+        System.out.println(list.removeBefore(50));
         System.out.println(list);
     }
 }
