1040c067b0d388f76dd75ce01d5e4e1f9956a40a
Nayan <nayan92001@gmail.com>
2024-07-18 15:34:56 +0530

    Tested ArrayStack using StackRunner

---
+++ Stack/StackRunner.java
@@ -0,0 +1,21 @@
+package Stack;
+
+import Stack.Interface.Stack;
+
+public class StackRunner {
+    public static void main(String[] args) throws Exception {
+        Stack<Integer> stack = new ArrayStack<>(10);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60);
+        System.out.println(stack);
+        System.out.println(stack.size());
+        stack.pop();
+        stack.pop();
+        System.out.println(stack);
+        System.out.println(stack.size());
+    }
+}
