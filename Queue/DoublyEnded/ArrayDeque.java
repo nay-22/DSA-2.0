0a399fb9fdf12d71e9ea6f3851235ee71103c4ec
Nayan <nayan92001@gmail.com>
2024-07-19 18:14:51 +0530

    Created Stubs for DoublyEnded types | TODO: Implement

---
+++ Queue/DoublyEnded/ArrayDeque.java
@@ -0,0 +1,73 @@
+package Queue.DoublyEnded;
+
+import Queue.Interface.Dequeue;
+
+public class ArrayDeque<T> implements Dequeue<T> {
+
+    @Override
+    public void offer(T data) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'offer'");
+    }
+
+    @Override
+    public void offerFirst(T data) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'offerFirst'");
+    }
+
+    @Override
+    public void offerLast(T data) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'offerLast'");
+    }
+
+    @Override
+    public T poll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'poll'");
+    }
+
+    @Override
+    public T pollFirst() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pollFirst'");
+    }
+
+    @Override
+    public T pollLast() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pollLast'");
+    }
+
+    @Override
+    public T peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+
+    @Override
+    public T peekFirst() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peekFirst'");
+    }
+
+    @Override
+    public T peekLast() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peekLast'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public boolean isFull() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isFull'");
+    }
+
+}
