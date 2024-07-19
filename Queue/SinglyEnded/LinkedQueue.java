cdbf9c3c73319047fc9a40a80b6d48c0a68e5b49
Nayan <nayan92001@gmail.com>
2024-07-19 18:13:57 +0530

    Created Stubs for SinglyEnded types | TODO: Implement

---
+++ Queue/SinglyEnded/LinkedQueue.java
@@ -0,0 +1,55 @@
+package Queue.SinglyEnded;
+
+import Queue.Interface.Queue;
+
+public class LinkedQueue<T> implements Queue<T> {
+
+    @Override
+    public void offer(T data) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'offer'");
+    }
+
+    @Override
+    public void offerLast(T data) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'offerLast'");
+    }
+
+    @Override
+    public T poll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'poll'");
+    }
+
+    @Override
+    public T pollFirst() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pollFirst'");
+    }
+
+    @Override
+    public T peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+
+    @Override
+    public T peekFirst() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peekFirst'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public boolean isFull() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isFull'");
+    }
+
+}
