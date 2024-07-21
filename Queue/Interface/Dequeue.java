fc40115ec700f20f98f8e4d11b92ac58eb170fd3
Nayan <nayan92001@gmail.com>
2024-07-21 20:50:06 +0530

    Added details about Exception

--- Queue/Interface/Dequeue.java
+++ Queue/Interface/Dequeue.java
@@ -4,21 +4,21 @@ public interface Dequeue<T> extends Queue<T> {
     /**
      * Inserts an element at the front of the Queue
      * @param data
-     * @throws Exception 
+     * @throws Exception Front space unavailability Exception
      */
     void offerFirst(T data) throws Exception;
 
     /**
      * Retrive the last inserted element from the Queue, while removing it as well
      * @return the last element in the Queue
-     * @throws Exception 
+     * @throws Exception Queue Underflow Exception
      */
     T pollLast() throws Exception;
 
     /**
      * Retrive the last inserted element from the Queue
      * @return return the last element in the Queue
-     * @throws Exception 
+     * @throws Exception Queue Underflow Exception
      */
     T peekLast() throws Exception;
 }
