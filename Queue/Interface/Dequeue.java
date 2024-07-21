4835989ba2ef86a408805d1ac14e22a476a132dd
Nayan <nayan92001@gmail.com>
2024-07-21 20:27:19 +0530

    Modified Dequeue interface to handle Exceptions

--- Queue/Interface/Dequeue.java
+++ Queue/Interface/Dequeue.java
@@ -4,18 +4,21 @@ public interface Dequeue<T> extends Queue<T> {
     /**
      * Inserts an element at the front of the Queue
      * @param data
+     * @throws Exception 
      */
-    void offerFirst(T data);
+    void offerFirst(T data) throws Exception;
 
     /**
      * Retrive the last inserted element from the Queue, while removing it as well
      * @return the last element in the Queue
+     * @throws Exception 
      */
-    T pollLast();
+    T pollLast() throws Exception;
 
     /**
      * Retrive the last inserted element from the Queue
      * @return return the last element in the Queue
+     * @throws Exception 
      */
-    T peekLast();
+    T peekLast() throws Exception;
 }
