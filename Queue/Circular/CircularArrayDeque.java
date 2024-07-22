1189e384b07e0725be14fb4729eb43d827250aed
Nayan <nayan92001@gmail.com>
2024-07-22 21:31:33 +0530

    Implemented CircularArrayDeque class

--- Queue/Circular/CircularArrayDeque.java
+++ Queue/Circular/CircularArrayDeque.java
@@ -2,76 +2,37 @@ package Queue.Circular;
 
 import Queue.Interface.Dequeue;
 
-public class CircularArrayDeque<T> implements Dequeue<T> {
-    @Override
-    public void offer(T data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'offer'");
+public class CircularArrayDeque<T> extends CircularArrayQueue<T> implements Dequeue<T> {
+
+    public CircularArrayDeque(int capacity) {
+        super(capacity);
     }
 
     @Override
-    public void offerFirst(T data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'offerFirst'");
+    public void offerFirst(T data) throws Exception {
+        if (isFull()) throw new Exception("Queue Overflow");
+        if (front == 0) front = capacity - 1;
+        else front = (front - 1) % capacity;
+        queue[front] = data;
     }
 
     @Override
-    public void offerLast(T data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'offerLast'");
+    public T pollLast() throws Exception {
+        if (isEmpty()) throw new Exception("Queue Undeflow");
+        T data = queue[rear];
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        }
+        else if (rear == 0) rear = capacity - 1;
+        else rear = (rear - 1) % capacity;
+        return data;
     }
 
     @Override
-    public T poll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'poll'");
+    public T peekLast() throws Exception {
+        if (isEmpty()) throw new Exception("Queue Undeflow");
+        return queue[rear];
     }
-
-    @Override
-    public T pollFirst() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'pollFirst'");
-    }
-
-    @Override
-    public T pollLast() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'pollLast'");
-    }
-
-    @Override
-    public T peek() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'peek'");
-    }
-
-    @Override
-    public T peekFirst() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'peekFirst'");
-    }
-
-    @Override
-    public T peekLast() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'peekLast'");
-    }
-
-    @Override
-    public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
-    }
-
-    @Override
-    public boolean isFull() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isFull'");
-    }
-
-    @Override
-    public int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
-    }
+    
 }
